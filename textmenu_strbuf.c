469980deab32c8708bac435405fdb11a1536f23f
Chekhov Ma <maqike@qq.com>
2021-10-28 18:44:57 +0800

    更新色盘代码，不再内置色盘数组

--- textmenu_strbuf.c
+++ textmenu_strbuf.c
@@ -1,7 +1,3 @@
 #include "textmenu_strbuf.h"
 
 menu_strBuf_t menu_dispStrBuf;
-
-#if defined(TEXTMENU_USE_PALETTE) && (TEXTMENU_USE_PALETTE > 0)
-menu_dispColor_t palette[TEXTMENU_DISPLAY_PALETTE_SIZE] = TEXTMENU_DISPLAY_PALETTE_DEF;
-#endif // ! TEXTMENU_USE_PALETTE
